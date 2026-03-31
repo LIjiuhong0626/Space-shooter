bcd384f7a01fbe78ab09a8a0f3eb556901342bc2
Jiu Hong <3348473469@qq.com>
2026-03-31 22:33:47 +0800

    石头的随机运动

--- code/main.py
+++ code/main.py
@@ -1,6 +1,74 @@
 import pygame
 from os.path import join
-from random import randint
+
+from random import randint, uniform
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('../', 'images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 ))
+        self.direction = pygame.Vector2()
+        self.speed = 300
+
+        #cooldown
+        self.can_shoot = True
+        self.laser_shoot_time = 0
+        self.cooldown_duration = 400
+
+    def laser_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks() #记录从init的时间
+            if current_time - self.laser_shoot_time >= self.cooldown_duration:
+                self.can_shoot = True
+
+    def update(self, dt): # 只有这个在一直运行
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt #速度是一致的， 我们要的就是帧率不同， 但是每秒钟移动的位移是一致的 , 而这个循环是每秒钟循环的次数， 循环次数乘dt是1
+
+        recent_keys = pygame.key.get_just_pressed()#如果不是这个， 那么在按下的同时会一直进行
+        if recent_keys[pygame.K_SPACE] and self.can_shoot:
+            Laser(laser_surf, self.rect.midtop, all_sprites)
+            self.can_shoot = False
+            self.laser_shoot_time = pygame.time.get_ticks()
+
+        self.laser_timer()
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, star_surf):
+        super().__init__(groups)
+        self.image = star_surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(midbottom = pos)
+
+    def update(self, dt):
+        self.rect.centery -= 400 * dt
+        if self.rect.bottom < 0:
+            self.kill()
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, surf, pos, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = pos)
+        self.start_time = pygame.time.get_ticks()
+        self.lifetime = 3000
+        self.direction = pygame.Vector2(uniform(-0.5, 0.5), 1) #可以有小数
+        self.speed = randint(400, 500)
+    
+    def update(self, dt):
+        self.rect.center += self.direction * self.speed * dt
+        current = pygame.time.get_ticks()
+        if current - self.start_time >= self.lifetime:
+            self.kill()
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -9,25 +77,22 @@ pygame.display.set_caption('Space shooter')
 running = True
 clock = pygame.time.Clock()
 
-# surface
-surf = pygame.Surface((100, 200)) #也是个内存
-surf.fill('orange')
-x = 100
-
-# imporing an image
-player_surf = pygame.image.load(join('../', 'images', 'player.png')).convert_alpha() #print(path) 防止mac和win的路径不同 #conver不用实时翻译
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 )) #只是返回一个位置信息， 但是是自动计算了的
-player_direction = pygame.math.Vector2(2, -1)
-player_speed = 100
-
-star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha() 
-star_positions = [(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)) for i in range(20)]#
-
+#inport
+star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()  #只需要一次
 meteor_surf = pygame.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
-meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 ))
-
 laser_surf = pygame.image.load(join('..', 'images', 'laser.png')).convert_alpha()
-laser_rect = laser_surf.get_rect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
+
+#sprites
+all_sprites = pygame.sprite.Group()
+for i in range(20):
+    Star(all_sprites, star_surf)
+player = Player(all_sprites)
+
+
+#custom event - > meteor event
+meteor_event = pygame.event.custom_type()
+pygame.time.set_timer(meteor_event, 500) #定个闹钟，每 500 毫秒发送一次这个信号。
 
 while running:
     dt = clock.tick(60) / 1000 #这一帧耗时了多少秒 默认单位是千毫秒
@@ -36,36 +101,16 @@ while running:
     for event in pygame.event.get():
         if event.type ==  pygame.QUIT:
             running = False
-        # if event.type == pygame.MOUSEMOTION:
-        #     player_rect.center = event.pos
+        if event.type == meteor_event:
+            x, y = randint(0, WINDOW_WIDTH), randint(-200, -100)
+            Meteor(meteor_surf, (x, y), all_sprites)
 
-    #input
-    #print(pygame.mouse.get_pressed()[0])
-    #print(pygame.mouse.get_rel())
-
-    key = pygame.key.get_pressed()
-    print(key[pygame.K_5])
+    #update
+    all_sprites.update(dt)#逻辑计算
 
     #draw the game
     display_surface.fill('darkgray')
-   
-    for pos in star_positions:
-        display_surface.blit(star_surf,pos)
-
-    display_surface.blit(laser_surf, laser_rect)
-    display_surface.blit(meteor_surf, meteor_rect)
-
-    #player movement
-    
-    player_rect.center += player_direction * (player_speed * dt) #速度是一致的， 我们要的就是帧率不同， 但是每秒钟移动的位移是一致的 , 而这个循环是每秒钟循环的次数， 循环次数乘dt是1
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1
-
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-
-    display_surface.blit(player_surf, player_rect) #盖章
-
+    all_sprites.draw(display_surface) #正式绘图
     pygame.display.update()
 
 pygame.quit()
