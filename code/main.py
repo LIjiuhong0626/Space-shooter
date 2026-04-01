f8bb6a819d7159ea341ae24b90f3c14452192fd4
Jiu Hong <3348473469@qq.com>
2026-04-01 14:35:53 +0800

    基本完成

--- code/main.py
+++ code/main.py
@@ -27,10 +27,11 @@ class Player(pygame.sprite.Sprite):
         self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
         self.rect.center += self.direction * self.speed * dt #速度是一致的， 我们要的就是帧率不同， 但是每秒钟移动的位移是一致的 , 而这个循环是每秒钟循环的次数， 循环次数乘dt是1
+                                        #保障帧率不同但是每秒移动位移相同
 
         recent_keys = pygame.key.get_just_pressed()#如果不是这个， 那么在按下的同时会一直进行
         if recent_keys[pygame.K_SPACE] and self.can_shoot:
-            Laser(laser_surf, self.rect.midtop, all_sprites)
+            Laser(laser_surf, self.rect.midtop, (all_sprites, laser_sprites))
             self.can_shoot = False
             self.laser_shoot_time = pygame.time.get_ticks()
 
@@ -69,6 +70,24 @@ class Meteor(pygame.sprite.Sprite):
         if current - self.start_time >= self.lifetime:
             self.kill()
 
+def collisions():
+    global running
+    collision_sprite = pygame.sprite.spritecollide(player, meteor_sprites, True)
+    if collision_sprite:#返回的是列表
+        running = False
+
+    for laser in laser_sprites:
+        collision_sprite = pygame.sprite.spritecollide(laser, meteor_sprites, True)#返回的是列表
+        if collision_sprite:
+            laser.kill()
+
+def display_score():
+    current_time = pygame.time.get_ticks() // 100
+    text_surf = font.render(str(current_time), True,(240, 240, 240)) #此时就是把文字变成图片
+    text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    pygame.draw.rect(display_surface, (240, 240, 240), text_rect.inflate(20, 16).move(0, -7), 5, 10)
+    display_surface.blit(text_surf, text_rect)
+
 # general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -81,10 +100,13 @@ clock = pygame.time.Clock()
 star_surf = pygame.image.load(join('..', 'images', 'star.png')).convert_alpha()  #只需要一次
 meteor_surf = pygame.image.load(join('..', 'images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('..', 'images', 'laser.png')).convert_alpha()
-
+font = pygame.font.Font(join('..', 'images', 'Oxanium-Bold.ttf'), 40) #你只是握着一支笔，选好了粗细和样式，但还没下笔。
 
 #sprites
 all_sprites = pygame.sprite.Group()
+meteor_sprites = pygame.sprite.Group()
+laser_sprites = pygame.sprite.Group()
+
 for i in range(20):
     Star(all_sprites, star_surf)
 player = Player(all_sprites)
@@ -103,14 +125,20 @@ while running:
             running = False
         if event.type == meteor_event:
             x, y = randint(0, WINDOW_WIDTH), randint(-200, -100)
-            Meteor(meteor_surf, (x, y), all_sprites)
+            Meteor(meteor_surf, (x, y), (all_sprites, meteor_sprites))
 
     #update
     all_sprites.update(dt)#逻辑计算
 
+    #collion test
+    collisions()
+
     #draw the game
-    display_surface.fill('darkgray')
+    display_surface.fill('#3a2e3f')
+    display_score()
     all_sprites.draw(display_surface) #正式绘图
+    
+
     pygame.display.update()
 
 pygame.quit()
